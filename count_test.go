a9f5eba544b47c89821e45a0581591cd0196ee83
z459ru <Z459z459z459>
2021-01-11 17:15:15 +0300

    initiate

--- count_test.go
+++ count_test.go
@@ -3,7 +3,7 @@ package count
 import "testing"
 
 func TestCount(t *testing.T) {
-	s := "qwertyeeu"
+	s := "qwertyeeud"
 	e := 3
 	if c := Count(s, 'e'); c != e {
 		t.Fatalf("bad  count for %s: got %d expected %d", s, c, e)
